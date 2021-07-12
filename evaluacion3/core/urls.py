ccdde0a9d96383a84e873b7bb1009c8bec1a69a2
k14121990 <k.villarroel39@gmail.com>
2021-07-11 23:54:55 -0400

    Update urls.py

--- evaluacion3/core/urls.py
+++ evaluacion3/core/urls.py
@@ -1,6 +1,13 @@
-from django.url import path
-from .views import home
+from django.urls import path
+from .views import home,form,form_libro,visualizacion
+
+
+
 
 urlpatterns = [
     path('', home,name="home"),
+    path('formulario', form, name="forma"),
+    path("Añadir", form_libro, name="añadir"),
+    path("visualizar", visualizacion, name="visualizar1"),
+
 ]
