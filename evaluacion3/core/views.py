f7dd556230c48f7a37e08acc04c55d6583f76c8d
k14121990 <k.villarroel39@gmail.com>
2021-07-11 23:54:59 -0400

    Update views.py

--- evaluacion3/core/views.py
+++ evaluacion3/core/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from django.template import loader
+from .models import libro,categoria_libro
+from django.forms import ModelForm
+from .forms import LibroForm
 
+def form(request):   
+    return render(request,'core/Formulario.html')
 
 
 def home(request):
-
+  
+    
     return render(request,'core/home.html')
+
+
+def form_libro(request):
+    datos ={ 'form': LibroForm()}
+    if request.method== 'POST':
+        Formulario = LibroForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Guardados correctamente"
+
+    return render(request,'core/form_libro.html', datos)
+
+
+def visualizacion(request):
+    libros = libro.objects.all()
+    datos ={ 'libros':libros}
+    return render(request,'core/MostratDtatos.html',datos)
+
+
+
